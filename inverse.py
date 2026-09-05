af07c5c9bb876a9d730e840e7fc87a109feb722d
anushkasharma292006-glitch <anushkasharma292006@gmail.com>
2026-09-05 10:44:15 +0530

    Delete inverse.py

--- inverse.py
+++
@@ -1,8 +0,0 @@
-#Inverse pattern
-n=int(input("Enter number:"))
-for i in range(5,0,-1):
-    for j in range(n-i):
-        print(" ",end="")
-    for j in range(2*i-1):
-        print("*",end="")
-    print() 
